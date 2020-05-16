a5f781dc7549926a4ac84f0d282538295adff19e
Vincenzo Pimpinella <v.pimpinella@proximanetwork.it>
2020-05-16 14:31:45 +0200

    update

---
+++ precache-manifest.1b67811cdbbeab615d528bae4ebaf784.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0172c0e5938801038133f059c767fb9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ae256ffed864635d0f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0cd6910d8ae0d18392a7",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "a2ae256ffed864635d0f",
+    "url": "/static/js/2.c7bf60e9.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.c7bf60e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd6910d8ae0d18392a7",
+    "url": "/static/js/main.a1481187.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "8f791f5dc7fd11f5a51903f9844b6146",
+    "url": "/static/media/CV-Europass-Pimpinella-IT.8f791f5d.pdf"
+  },
+  {
+    "revision": "1be887489d648bb95c0a6d03f4dbef66",
+    "url": "/static/media/avatar.1be88748.png"
+  }
+]);
